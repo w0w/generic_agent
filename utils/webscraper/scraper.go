3e359e7cab2383a51e07c9891b1222fa8fe4e332
Naveen Singh <io.naveens@gmail.com>
2025-01-06 00:49:07 +0700

    Final commit Week#1

--- utils/webscraper/scraper.go
+++ utils/webscraper/scraper.go
@@ -2,13 +2,13 @@ package webscraper
 
 import (
     "github.com/PuerkitoBio/goquery"
-    "anondd/utils/models"
+    "anondd/utils/storage"
 )
 
 // Scraper defines the interface for scrapers
 type Scraper interface {
     FetchHTML(endpoint string) (*goquery.Document, error)
-    ParseAgents(doc *goquery.Document) ([]models.Agent, error)
-    GetAgent(id string) (*models.Agent, error)
+    ScrapeAgents() error
+    GetStore() *storage.AgentStore
     StopScheduler()
 }
